f2710d71277dc9cf9900775899dec42d2aaa6151
YakuzaDoggo <jettblc@outlook.com>
2023-07-17 20:28:25 -0400

    added ability to remove a channel (beta)

--- cogs/cogs.py
+++ cogs/cogs.py
@@ -14,32 +14,47 @@ class user(commands.Cog):
 
     @commands.command(name="start")
     async def start_sending(self, ctx):
-            chID = ctx.channel.id
+        chID = ctx.channel.id
 
-            for x in Tumbleweed.ChList:
-                if x == chID:
-                    await ctx.send("This channel " + "\(" + str(ctx.channel.id) + "\)" + " is already in the posting list!")
-                    return
+        for x in Tumbleweed.ChList:
+            if x == chID:
+                await ctx.send("This channel " + "\(" + str(ctx.channel.id) + "\)" + " is already in the posting list!")
+                return
 
-            Tumbleweed.ChList.append(ctx.channel.id)
-            await ctx.send("Add this channel to the posting list!")
-            await sleep(1)
-            await ctx.send("Note that you can't remove a channel yet.\nNor will each channel have a unique timer (this might be annoying if you use this command in several channels)")
+        Tumbleweed.ChList.append(chID)
+        await ctx.send("Add this channel to the posting list!")
+        await sleep(1)
+        await ctx.send("Note that you can't remove a channel yet.\nNor will each channel have a unique timer (this might be annoying if you use this command in several channels)")
 
-    @nextcord.slash_command(name = "start", description = "Add current Channel ID to the posting list.")
+    @commands.command(name="stop")
+    async def stop_sending(self, ctx):
+        chID = ctx.channel.id
+
+        Tumbleweed.ChList.remove(chID)
+        await ctx.send("Remove this channel from the posting list!")
+
+
+    @nextcord.slash_command(name = "start", description = "Add current channel ID to the posting list.")
     async def start_sending(self, ctx):
-            chID = ctx.channel.id
+        chID = ctx.channel.id
 
-            for x in Tumbleweed.ChList:
-                if x == chID:
-                    await ctx.send("This channel " + "\(" + str(ctx.channel.id) + "\)" + " is already in the posting list!")
-                    
-                    return
+        for x in Tumbleweed.ChList:
+            if x == chID:
+                await ctx.send("This channel " + "\(" + str(ctx.channel.id) + "\)" + " is already in the posting list!")
+                
+                return
 
-            Tumbleweed.ChList.append(ctx.channel.id)
-            await ctx.send("Add this channel to the posting list!")
-            await sleep(1)
-            await ctx.send("Note that you can't remove a channel yet.\nNor will each channel have a unique timer (this might be annoying if you use this command in several channels)")
+        Tumbleweed.ChList.append(ctx.channel.id)
+        await ctx.send("Add this channel to the posting list!")
+        await sleep(1)
+        await ctx.send("Note that you can't remove a channel yet.\nNor will each channel have a unique timer (this might be annoying if you use this command in several channels)")
+
+    @nextcord.slash_command(name = "stop", description = "Remove current channel ID from the posting list")
+    async def stop_sending(self, ctx):
+        chID = ctx.channel.id
+         
+        Tumbleweed.ChList.remove(chID)
+        await ctx.send("Remove this channel from the posting list!")
 
 def setup(bot):
   bot.add_cog(user(bot))
