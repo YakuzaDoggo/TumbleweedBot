2797768b321fc28d53cec50977a4fcfc89a7b326
Dorg <32146176+YakuzaDoggo@users.noreply.github.com>
2024-07-18 11:58:16 -0400

    Update functions.py
    added comments

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+# Before using this code for any reason (running, editing) please read the license included in the LICENSE folder
+
 import sys
 import os
 from os.path import isfile, join
@@ -6,6 +8,8 @@ sys.path.insert(0, './images/')
 import pathlib
 import images
 
+# Array that sifts through the images folder located in ./images/ to add them to an array
+# ran at start of the bot
 def UpdateArray():
     directory = pathlib.Path('./images/images')
     for currentFile in directory.iterdir():
